b0a5a0f52cc17e521e47d5ed04e994722ca74633
Gleb <aleshkevichgleb05@gmail.com>
2023-10-07 16:08:45 +0300

    add: db.json

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,16 +1,28 @@
-import {FC} from 'react';
+import {EventHandler, FC} from 'react';
 import { postAPI } from '../services/PostService';
 import PostItem from './PostItem';
+import { IPost } from '../types/types';
 
 const PostContainer:FC = () => {
-    const {data: posts, isLoading, isError, error} = postAPI.useFetchAllPostsQuery(10);
+    const {data: posts, isLoading, isError, error} = postAPI.useFetchAllPostsQuery(200);
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation();
+    const [updatePost, {}] = postAPI.useUpdatePostMutation();
+
+    const handleNewPost = async() => {
+        const title = prompt();
+        await createPost({title, body: title} as IPost)
+    }
+    
+    
     return (
         <div>
+            <button onClick={handleNewPost}>Add new Post</button>
             {isError && <h3>fdfdg</h3>}
             {isLoading && <h3>Loading...</h3>}
             {
                 posts?.map(post => 
-                    <PostItem key={post.id} post = {post}/>    
+                    <PostItem key={post.id} post = {post} remove={deletePost}  update={updatePost}/>    
                 )
             }
         </div>
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -3,14 +3,25 @@ import { IPost } from '../types/types';
 
 interface PostItemProps {
     post: IPost,
+    remove: (post: IPost) => void,
+    update: (post: IPost) => void,
 }
 
-const PostItem: FC<PostItemProps> = ({post}) => {
+const PostItem: FC<PostItemProps> = ({post, remove, update}) => {
+    const handleUpdate = () => {
+        const title = prompt();
+        update({title, body: title, id: post.id} as IPost);
+    }
+
+    const handleDelete = (event: React.MouseEvent) => {
+        event.stopPropagation();
+        remove(post)
+    }
 
     return (
-        <div>
+        <div onClick={handleUpdate} style={{padding: '20px', border: '1px solid black'}}>
             {post.id} {post.title}
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     );
 };
